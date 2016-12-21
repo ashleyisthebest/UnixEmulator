df79ff537d6ecb7b36adb929c4cd17ad946f6734
ohlynx <ashleybest02@gmail.com>
2016-12-21 16:22:00 +0000

    Started ping command

---
+++ LinuxTerminalEmulator/LinuxTerminalEmulator/ping.vb
@@ -0,0 +1,14 @@
+﻿Module ping
+
+    Public Sub ping()
+        If command.Contains("ping ") Then
+            'Recognising command
+            recongnized = True
+            'Removing "ping  "
+            command = command.Remove(0, 5)
+
+
+        End If
+    End Sub
+
+End Module
